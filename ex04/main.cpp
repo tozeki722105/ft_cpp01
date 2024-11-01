4c6ba35a184bbdd8e49a9c336d9d8299ef51dd0b
toshihiro ozeki <you@example.com>
2024-11-02 07:55:35 +0900

    update: main.cpp

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,35 +1,35 @@
+#include <fstream>
 #include <iostream>
 #include <string>
-#include <fstream>
 
-std::string convert(std::string &content, const std::string &old_str, 
-		const std::string &new_str)
+std::string convert(std::string &content, const std::string &old_str, const std::string &new_str)
 {
-	size_t pos;
 	size_t i = 0;
-	while ((pos = content.find(old_str, i)) != std::string::npos)
-	{
+	size_t pos = content.find(old_str, i);
+	while (pos != std::string::npos) {
 		content.erase(pos, old_str.length());
 		content.insert(pos, new_str);
 		i = pos + new_str.length();
+		pos = content.find(old_str, i);
 	}
 	return content;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 	if (argc != 4)
 		return 1;
-    std::ifstream ifs(argv[1]);
+	std::ifstream ifs(argv[1]);
 	std::ofstream ofs(std::string(argv[1]) + ".replace");
-    if (!ifs || !ofs) {
-        std::cerr << "Error opening file." << std::endl;
-        return 1;   
-    }
+	if (!ifs || !ofs) {
+		std::cerr << "Error opening file." << std::endl;
+		return 1;
+	}
 	std::string buffer;
-	if(!std::getline(ifs, buffer, '\0'))
+	if (!std::getline(ifs, buffer, '\0'))
 		return 1;
 	// std::cout << "before" << std::endl << buffer << std::endl;
 	convert(buffer, argv[2], argv[3]);
 	// std::cout << "after" << std::endl<< buffer << std::endl;
-	ofs << buffer;	
+	ofs << buffer;
 }
