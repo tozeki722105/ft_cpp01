0da285fe25615e84a79b350c3c7d9b07bd7ae9fd
toshihiro ozeki <you@example.com>
2024-11-02 03:13:38 +0900

    clean: format ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,15 +3,11 @@
 
 int main()
 {
-	std::string str = "HI THIS IS BRAIN";
+	std::string  str = "HI THIS IS BRAIN";
 	std::string *stringPTR = &str;
 	std::string &stringREF = str;
 
-	std::cout << std::hex << &str << std::endl 
-		<< stringPTR << std::endl 
-		<< &stringREF << std::endl;
+	std::cout << std::hex << &str << std::endl << stringPTR << std::endl << &stringREF << std::endl;
 	std::cout << std::endl;
-	std::cout << str << std::endl 
-		<< *stringPTR << std::endl 
-		<< stringREF << std::endl;
+	std::cout << str << std::endl << *stringPTR << std::endl << stringREF << std::endl;
 }
